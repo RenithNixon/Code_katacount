65664dbaab64e4838a31f6a01734e1599e6bb25e
RenithNixon <renithreni95@gmail.com>
2016-10-21 11:15:09 +0530

    Update CountDigits.java

--- CountDigits.java
+++ CountDigits.java
@@ -6,6 +6,8 @@ public static void main(String args[])
 int n,count=0,rem=0;
 Scanner sc=new Scanner(System.in);
 n=sc.nextInt();
+if(n>0)
+{
 while(n!=0)
 {
 rem=n%10;
@@ -13,6 +15,11 @@ count++;
 n=n/10;
 }
 System.out.println("The number of digits in the given integer is:"+count);
+}
+else
+{
+System.out.println("Invalid Data");
+}
 }
 }
 
